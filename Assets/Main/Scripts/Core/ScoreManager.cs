04db3b7ce3019644efd9700e802617516b5c3878
TenmaUchiyama <tenmaimp@gmail.com>
2024-01-07 23:51:17 +0900

    Added Ranking Display System

--- Assets/Main/Scripts/Core/ScoreManager.cs
+++ Assets/Main/Scripts/Core/ScoreManager.cs
@@ -13,6 +13,10 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI secondText;
     [SerializeField] private TextMeshProUGUI thirdText;
     [SerializeField] private TextMeshProUGUI fourthText;
+    
+    [SerializeField] private TextMeshProUGUI rankText; 
+    
+    
 
     private int _rank;
 
@@ -20,6 +24,7 @@ public class ScoreManager : MonoBehaviour
     
     public void Start()
     {
+        
         int scoreData = DataHolder.PassCountHolder; 
      
         ScoreData newScoreData = new ScoreData()
@@ -31,17 +36,20 @@ public class ScoreManager : MonoBehaviour
         ScoreDataList scoreDataListObject = _savingSystem.UpdateScoreData(newScoreData);
 
         List<ScoreData> scoreDataList = scoreDataListObject.scoredataList; 
-    
+        
+        
+        
+        
+        
         //Compare and search the rank
         for (int i = 0; i < scoreDataList.Count; i++)
         {
 
-    
+            rankText.text += $"{i + 1}   {scoreDataList[i].score}     {scoreDataList[i].savedDate}\n";
             if (scoreDataList[i].score == newScoreData.score &&
                 scoreDataList[i].savedDate == newScoreData.savedDate)
             {
                 _rank = i + 1;
-                break;
             }
         }
 
@@ -58,9 +66,13 @@ public class ScoreManager : MonoBehaviour
        if (scoreDataList.Count < 2) return; 
        
        thirdText.text = $"3rd: {scoreDataList[2].score}  {scoreDataList[2].savedDate}";
-       
-       fourthText.text = $"Your Score: {newScoreData.score}  {newScoreData.savedDate}";
 
+       string rankDislay = _rank.ToString();
+       if (_rank == 0) rankDislay = "Out of Rank";
+       fourthText.text = $"Your Rank:   {rankDislay}        {newScoreData.score}  {newScoreData.savedDate}";
+        
+       
+       
 
 
 
